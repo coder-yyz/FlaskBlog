28a048358df1d06fb22e1e567880b545886a3270
coder-yyz <2562874272@qq.com>
2019-02-03 15:25:56 +0800

    ######

--- app/models.py
+++ app/models.py
@@ -37,7 +37,7 @@ class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
